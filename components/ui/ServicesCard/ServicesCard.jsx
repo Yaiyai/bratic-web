9d41a9728631e4532237147733d904ae2f0186a1
Yai <myd.rio@hotmail.com>
2021-02-28 12:24:26 +0100

    services section in services page

--- components/ui/ServicesCard/ServicesCard.jsx
+++ components/ui/ServicesCard/ServicesCard.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCheckCircle } from '@fortawesome/free-solid-svg-icons'
-const ServicesCard = ({ bkg, features, title, especialClass }) => {
+
+const ServicesCard = ({ bkg, features, title, specialClass }) => {
 	let imageCard = {
 		backgroundImage: `url(${bkg})`,
 	}
 	return (
-		<article style={imageCard} className={'servicios-card ' + especialClass}>
+		<article className='servicios-card'>
+			<span className={`${specialClass}`} style={imageCard}></span>
 			<h3>{title}</h3>
 			<ul>
 				{features?.map((ft, idx) => (
