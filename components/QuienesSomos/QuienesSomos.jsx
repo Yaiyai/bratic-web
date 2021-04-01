d863916e7370366539597a18411d154074f47aca
Yai <myd.rio@hotmail.com>
2021-04-01 20:14:44 +0200

    wip responsive

--- components/QuienesSomos/QuienesSomos.jsx
+++ components/QuienesSomos/QuienesSomos.jsx
@@ -44,32 +44,29 @@ const QuienesSomos = () => {
 				</div>
 			</section>
 			<section id='somos-features'>
-				<h2>¿Por qué bratic? / ¿Qué nos diferencia?</h2>
-				<div className='features bratic-container'>
-
-					<FeatureCard
-						delay={ 0 }
-						title={ 'Equipo' }
-						image='https://res.cloudinary.com/bratic-app/image/upload/v1617278455/web/draw-team_uda23m.svg'
-						text={ 'Somos un equipo venido del mundo empresarial, con experiencia real sobre el terreno, y conocimientos técnicos *****.' }
-					/>
-
-
-					<FeatureCard
-						delay={ 500 }
-						title={ 'Experiencia' }
-						image='https://res.cloudinary.com/bratic-app/image/upload/v1617278455/web/draw-experience_at9hrn.svg'
-						text={ 'La experiencia de siempre, formando la industria del futuro:' }
-						list={ features }
-					/>
-
-
-					<FeatureCard
-						delay={ 1000 }
-						title={ 'Camino' }
-						image='https://res.cloudinary.com/bratic-app/image/upload/v1617278455/web/draw-camino_whodr7.svg'
-						text={ 'Además, recorreremos junto a ti todo el camino, desde que se origina la necesidad hasta cerrar el ciclo con la puesta en marcha.' }
-					/>
+				<div className="bratic-container">
+					<h2>¿Por qué bratic? / ¿Qué nos diferencia?</h2>
+					<div className='features bratic-container'>
+						<FeatureCard
+							delay={ 0 }
+							title={ 'Equipo' }
+							image='https://res.cloudinary.com/bratic-app/image/upload/v1617278455/web/draw-team_uda23m.svg'
+							text={ 'Somos un equipo venido del mundo empresarial, con experiencia real sobre el terreno, y conocimientos técnicos *****.' }
+						/>
+						<FeatureCard
+							delay={ 500 }
+							title={ 'Experiencia' }
+							image='https://res.cloudinary.com/bratic-app/image/upload/v1617278455/web/draw-experience_at9hrn.svg'
+							text={ 'La experiencia de siempre, formando la industria del futuro:' }
+							list={ features }
+						/>
+						<FeatureCard
+							delay={ 1000 }
+							title={ 'Camino' }
+							image='https://res.cloudinary.com/bratic-app/image/upload/v1617278455/web/draw-camino_whodr7.svg'
+							text={ 'Además, recorreremos junto a ti todo el camino, desde que se origina la necesidad hasta cerrar el ciclo con la puesta en marcha.' }
+						/>
+					</div>
 				</div>
 			</section>
 		</>
--- components/Services/Services.jsx
+++ components/Services/Services.jsx
@@ -3,18 +3,23 @@ import Card from '../ui/Card/Card'
 
 const Services = () => {
 	return (
-		<section id='services-section' className='bratic-container'>
-			<h2>¿Qué podemos hacer por tu empresa?</h2>
+		<section id='services-section'>
 
-			<div>
-				<Card delay={ 0 }
-					title='Consultoría digital'
-					text='Diagnóstico y asesoramiento experto para transformar tu empresa a través de la tecnología, con soluciones adaptadas a tus necesidades.'
-					link='/servicios'
-				/>
-				<Card delay={ 250 } title='Industria 4.0' text='Análisis, definición e implantación de soluciones específicas que optimicen los procesos de empresas industriales y logísticas.' link='/servicios' />
-				<Card delay={ 500 } title='Ciberseguridad' text='Analizamos y proponemos las herramientas y procesos que aseguren a tu empresa ante posibles ataques externos.' link='/servicios' />
-				<Card delay={ 750 } title='Formación' text='Formación específica para asegurar las competencias necesarias que conviertan a las personas en impulsoras del cambio.' link='/servicios' />
+			<div className="bratic-container">
+				<h2>¿Qué podemos hacer por tu empresa?</h2>
+			</div>
+
+			<div className='bratic-container'>
+				<div className="all-cards">
+					<Card delay={ 0 }
+						title='Consultoría digital'
+						text='Diagnóstico y asesoramiento experto para transformar tu empresa a través de la tecnología, con soluciones adaptadas a tus necesidades.'
+						link='/servicios'
+					/>
+					<Card delay={ 250 } title='Industria 4.0' text='Análisis, definición e implantación de soluciones específicas que optimicen los procesos de empresas industriales y logísticas.' link='/servicios' />
+					<Card delay={ 500 } title='Ciberseguridad' text='Analizamos y proponemos las herramientas y procesos que aseguren a tu empresa ante posibles ataques externos.' link='/servicios' />
+					<Card delay={ 750 } title='Formación' text='Formación específica para asegurar las competencias necesarias que conviertan a las personas en impulsoras del cambio.' link='/servicios' />
+				</div>
 			</div>
 		</section>
 	)
