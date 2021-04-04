4aec47294ccbe04ef71e6f9398ae0ffa90ca0a72
Yai <myd.rio@hotmail.com>
2021-04-04 18:44:55 +0200

    gestion de ayudas cms

--- components/GestionAyudas/GestionAyudas.jsx
+++ components/GestionAyudas/GestionAyudas.jsx
@@ -5,7 +5,7 @@ import { faCheckCircle } from '@fortawesome/free-solid-svg-icons'
 import { Animated } from 'react-animated-css'
 import { isInViewport } from '../../helpers/isInViewport.helper'
 
-const GestionAyudas = () => {
+const GestionAyudas = ({ gestionAyudas }) => {
 	const animatedImage = useRef()
 	const [imageIsInView, setImageIsInView] = useState(false)
 
@@ -15,16 +15,8 @@ const GestionAyudas = () => {
 		})
 	}, [isInViewport])
 
-	let quienesSomosImage = 'https://res.cloudinary.com/bratic-app/image/upload/v1617037439/web/Image-squares.png'
-	let ayudas = [
-		'Ayudas a la I+D+i.',
-		'ACTIVA Industria 4.0.',
-		'Ayudas CDTI.',
-		'Ayudas INNOVA COVID.',
-		'Ayudas CRECE.',
-		'Ayudas Fondo Europeo de Desarrollo Regional (FEDER).',
-		'Ayudas a la digitalización fondos europeos Recuperación y la Resiliencia y programa ­React-EU.',
-	]
+	let quienesSomosImage = gestionAyudas.uniqueImage
+
 	return (
 		<section id='gestion-ayudas'>
 			<div className="bratic-container">
@@ -32,17 +24,8 @@ const GestionAyudas = () => {
 					<img ref={ animatedImage } src={ quienesSomosImage } alt='' />
 				</Animated>
 				<article className='right'>
-					<h2>Gestión de ayudas</h2>
-					<p>Ayudamos a las empresas a aprovechar las oportunidades que ofrecerán los fondos europeos de recuperación.</p>
-					<p>Contamos con amplia experiencia en la búsqueda y tramitación de Financiación y Ayudas regionales, estatales y europeas:</p>
-					<ul>
-						{ ayudas?.map((ay, idx) => (
-							<li key={ idx }>
-								<FontAwesomeIcon icon={ faCheckCircle } />
-								{ay }
-							</li>
-						)) }
-					</ul>
+					<h2>{ gestionAyudas.title }</h2>
+					<div dangerouslySetInnerHTML={ gestionAyudas.parsedText }></div>
 				</article>
 			</div>
 		</section>
--- pages/servicios/index.jsx
+++ pages/servicios/index.jsx
@@ -9,7 +9,7 @@ import Services from '../../components/Services/ServicesPage/Services'
 import PageHeader from '../../components/ui/PageHeader/PageHeader'
 import BraticLayout from '../../layout/BraticLayout'
 
-const ServiciosPage = ({ companyFetched, header, contacto, servicios, metodologia, comoTrabajamos }) => {
+const ServiciosPage = ({ companyFetched, header, contacto, servicios, metodologia, comoTrabajamos, gestionAyudas }) => {
 	return (
 		<BraticLayout>
 			<PageHeader bkg={ header.uniqueImage } title={ header.title } />
@@ -17,7 +17,7 @@ const ServiciosPage = ({ companyFetched, header, contacto, servicios, metodologi
 				<Services servicios={ servicios } />
 				<Metodologia metodologia={ metodologia } />
 				<ComoTrabajamos comoTrabajamos={ comoTrabajamos } />
-				<GestionAyudas />
+				<GestionAyudas gestionAyudas={ gestionAyudas } />
 				<Contact contacto={ contacto } company={ companyFetched } />
 			</section>
 		</BraticLayout>
@@ -25,15 +25,16 @@ const ServiciosPage = ({ companyFetched, header, contacto, servicios, metodologi
 }
 
 export const getServerSideProps = async () => {
-	const [companyFetched, header, contacto, servicios, metodologia, comoTrabajamos] = await Promise.all([
+	const [companyFetched, header, contacto, servicios, metodologia, comoTrabajamos, gestionAyudas] = await Promise.all([
 		getCompany(),
 		getSection('6069d5237844ac00158eedc3'),
 		getSection('6069d3f47844ac00158eedc2'),
 		getSection('6069dfe9165c6f00159f27f0'),
 		getSection('6069e314165c6f00159f27f1'),
-		getSection('6069e633165c6f00159f27f2')
+		getSection('6069e633165c6f00159f27f2'),
+		getSection('6069eb32165c6f00159f27f4')
 	])
-	return { props: { companyFetched, header, contacto, servicios, metodologia, comoTrabajamos } }
+	return { props: { companyFetched, header, contacto, servicios, metodologia, comoTrabajamos, gestionAyudas } }
 }
 
 export default ServiciosPage
