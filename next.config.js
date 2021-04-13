6a40805c9302da84d8f083591b9e2babb20cc72a
Yai <myd.rio@hotmail.com>
2021-04-13 16:06:07 +0200

    email service

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ module.exports = {
 	distDir: 'build',
 	env: {
 		baseURL: 'https://bratic-app.herokuapp.com/api',
-		serviceEmail: 'bratic_contact',
+		serviceEmail: 'contacto_hola_bratic',
 		templateEmail: 'bratic_contact_template',
 		userEmail: 'user_ktgYq5LuGt8HgLfoJW8MI',
 	},
