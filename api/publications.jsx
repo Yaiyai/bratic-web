a05c0cb2426078a8fca82af960711fba792cecb7
Yai <myd.rio@hotmail.com>
2021-06-03 18:46:47 +0200

    posts ordered by date

--- api/publications.jsx
+++ api/publications.jsx
@@ -1,13 +1,17 @@
+import dayjs from 'dayjs'
+
 export const getPublications = () => {
     return fetch(`${process.env.baseURL}/posts`)
         .then(data => data.json())
         .then(posts => posts.posts.filter(post => post.status === 'publicado'))
+        .then(posts => posts.sort((a, b) => new Date(b.postDate) - new Date(a.postDate)))
         .catch(err => new Error(err))
 }
 export const getMorePublications = () => {
-    return fetch(`${process.env.baseURL}/posts?limit=4`)
+    return fetch(`${process.env.baseURL}/posts`)
         .then(data => data.json())
         .then(posts => posts.posts.filter(post => post.status === 'publicado'))
+        .then(posts => posts.sort((a, b) => new Date(b.postDate) - new Date(a.postDate)).splice(0, 4))
         .catch(err => new Error(err))
 }
 
@@ -15,6 +19,7 @@ export const getPublicationsByCategory = (category) => {
     return fetch(`${process.env.baseURL}/posts/category/${category}`)
         .then(data => data.json())
         .then(posts => posts.posts.filter(post => post.status === 'publicado'))
+        .then(posts => posts.sort((a, b) => new Date(b.postDate) - new Date(a.postDate)))
         .catch(err => new Error(err))
 }
 
