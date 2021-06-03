9d09192a5c3ca68333b47573a6bfbe4a3debf3ea
Yai <myd.rio@hotmail.com>
2021-06-03 18:09:39 +0200

    added erased files

---
+++ api/company.jsx
@@ -0,0 +1,7 @@
+export const getCompany = () => {
+    const apiURL = process.env.baseURL
+    return fetch(`${apiURL}/companies`)
+        .then((data) => data.json())
+        .then((companies) => companies.company[0])
+        .catch((err) => new Error(err))
+}
---
+++ api/publications.jsx
@@ -0,0 +1,25 @@
+export const getPublications = () => {
+    return fetch(`${process.env.baseURL}/posts`)
+        .then(data => data.json())
+        .then(posts => posts.posts.filter(post => post.status === 'publicado'))
+        .catch(err => new Error(err))
+}
+export const getMorePublications = () => {
+    return fetch(`${process.env.baseURL}/posts?limit=4`)
+        .then(data => data.json())
+        .then(posts => posts.posts.filter(post => post.status === 'publicado'))
+        .catch(err => new Error(err))
+}
+
+export const getPublicationsByCategory = (category) => {
+    return fetch(`${process.env.baseURL}/posts/category/${category}`)
+        .then(data => data.json())
+        .then(posts => posts.posts.filter(post => post.status === 'publicado'))
+        .catch(err => new Error(err))
+}
+
+export const getPostBySlug = (slug) => {
+    return fetch(`${process.env.baseURL}/posts/post/slug/${slug}`)
+        .then(data => data.json())//Ojo que con esta respuesta veo si tiene el redirect
+        .catch(err => new Error(err))
+}
